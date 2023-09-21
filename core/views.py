2bf6d3e05fa44a9d5c88056bb6dc95e4f2bfb051
Nero <jesalemalo@gmail.com>
2023-09-21 12:32:01 -0400

    Add token for user and session for user with only one token

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
+from django.contrib.sessions.models import Session
+
 
 from .serializers import MyUser, MyUserSerializerGET, MyUserSerializerPOST
 
@@ -7,7 +9,8 @@ from rest_framework.views import APIView
 from rest_framework import exceptions, status
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
-
+from rest_framework.authtoken.models import Token
+from datetime import datetime
 # Create your views here.
 
 # Clase para enlistar(GET) a todos los usuarios regstrados en la API.
@@ -33,5 +36,32 @@ class Login(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
         login_serializer = self.serializer_class(data=request.data, context = {"request":request})
         if login_serializer.is_valid():
-            print("La validacion paso con exito")
+            user = login_serializer.validated_data['user']
+            if user.is_active:
+                token, created = Token.objects.get_or_create(user = user)
+                user_serializer = MyUserSerializerGET(user)
+                if created:
+                    return Response({'token': token.key,
+                                     'user': user_serializer.data,
+                                     'message': 'Inicio de sesion exitoso.'}, status=status.HTTP_201_CREATED)
+                else:
+                    all_sessions = Session.objects.filter(expire_date__gte = datetime.now())
+                    if all_sessions.exists():
+                        for session in all_sessions:
+                            session_data = session.get_decoded()
+                            if user.id == int(session_data.get('_auth_user_id')):
+                                session.delete()
+                    token.delete()
+                    token = Token.objects.create(user = user)
+                    return Response({'token': token.key,
+                                     'user': user_serializer.data,
+                                     'message': 'Inicio de sesion exitoso.'}, status=status.HTTP_201_CREATED)
+                
+
+            else:
+                return Response({'error':"Este usuario no puede iniciar sesion"}, status=status.HTTP_401_UNAUTHORIZED)
+            
+        else:
+            return Response({'error': 'Los datos ingresados son incorrectos'}, status=status.HTTP_400_BAD_REQUEST)
+        
         return Response({'message':"Hola desde response"}, status=status.HTTP_200_OK)
