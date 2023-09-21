139fe26308394b0df05ac06d443447660daf14b1
Nero <jesalemalo@gmail.com>
2023-09-21 09:43:43 -0400

    Add a new custom manager on models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+
+class MyManager(BaseUserManager):
+
+    def create_user(self, email, username, password = None):
+
+        if not email:
+            raise ValueError('The user need a email value')
+        user = self.model(
+            email = self.normalize_email(email=email),
+            username = username,
+        )
+
+        user.set_password(password)
+        user.save()
+        return user
+    
+
+    def create_superuser(self, email, username, password):
+
+        user = self.create_user(
+            email,
+            username = username,
+            password = password,
+        )
+
+        user.is_admin = True
+        user.save()
+        return user
+    
+
+
