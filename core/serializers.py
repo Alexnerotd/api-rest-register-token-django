a97ca9f897d6dc025a7b9a27d6732fda7e44d8dc
Nero <jesalemalo@gmail.com>
2023-09-21 10:50:15 -0400

    Add two serializers, first one is for request GET and second is for request POST and encrypt password

--- core/serializers.py
+++ core/serializers.py
@@ -1,1 +1,22 @@
 from rest_framework import serializers
+from .models import MyUser
+
+class MyUserSerializerGET(serializers.ModelSerializer):
+
+    class Meta:
+        model = MyUser
+        fields = ['username', 'email', 'name', 'last_name']
+
+
+class MyUserSerializerPOST(serializers.ModelSerializer):
+
+    class Meta: 
+        model = MyUser
+        fields = ['username', 'email', 'password', 'name', 'last_name']
+
+
+    def create(self, validate_data):
+        user = MyUser(**validate_data)
+        user.set_password(validate_data['password'])
+        user.save()
+        return user
