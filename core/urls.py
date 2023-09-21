407fdb65a2fe231d803056d5eeaae7046da6a7bc
Nero <jesalemalo@gmail.com>
2023-09-21 11:11:43 -0400

    Add view GET for list all users

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import APIListUsersView
 
 
 
 urlpatterns = [
+    path('api/users/', APIListUsersView.as_view(), name='users-list'),
     
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import Http404
+
+from .serializers import MyUser, MyUserSerializerGET, MyUserSerializerPOST
+
+from rest_framework.views import APIView
+from rest_framework import exceptions, status
+from rest_framework.response import Response
 
 # Create your views here.
+class APIListUsersView(APIView):
+
+    def get(self, request, format = None):
+        user = MyUser.objects.all()
+        user_serializer = MyUserSerializerGET(user, many = True)
+        try:
+            if user_serializer:
+                return Response(user_serializer.data, status=status.HTTP_200_OK)
+        except MyUser.DoesNotExist:
+            return Response({'message': 'No existen usuario registrados hasta el momento'}, status=status.HTTP_404_NOT_FOUND)
+        
